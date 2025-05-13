9edfbe78de80a33719364ac0fe5011cf0a7bf425
m-rahimi98 <161834474+m-rahimi98@users.noreply.github.com>
2025-05-13 20:00:31 +0330

    pair_train.py

--- pair_train.py
+++ pair_train.py
@@ -104,6 +104,7 @@ if __name__ == "__main__":
     if torch.cuda.device_count() > 1:
         print(f"Using {torch.cuda.device_count()} GPUs")
         model = torch.nn.DataParallel(model)
+    model = model.to(device)
     
     # اگر فایل checkpoint موجوده، state_dict رو لود کن
     if os.path.isfile(f):
